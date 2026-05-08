7a599854ce3b9395e580bd47d7eb91898039bc4d
alessblaze <alessblaze@users.noreply.github.com>
2026-05-08 17:26:42 +0000

    deploy: cf4f7dd37b2cc032d73e210ad2f98d2d218e606b

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-368c2642.js',
+                path_to_root + 'searchindex-fcecde23.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
